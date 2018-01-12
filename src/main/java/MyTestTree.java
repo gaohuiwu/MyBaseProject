9af86baf177260d63cfce022ac0b2a53fdb45ec7
gaohw <ghw0614@126.com>
2018-01-12 11:15:26 +0800

    测试

---
+++ src/main/java/MyTestTree.java
@@ -0,0 +1,15 @@
+/**
+ * FileName:MyTestTree
+ * Author:  ghw06
+ * Date:    2018/1/12 11:11
+ * Description: 测试下resource tree 分支
+ */
+public class MyTestTree {
+    private int id;
+    private String name;
+
+    public MyTestTree(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
