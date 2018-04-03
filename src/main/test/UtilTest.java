6c7225544c86aece7ada1a32a631dc237196c0a9
gaohw <ghw0614@126.com>
2018-04-03 23:36:16 +0800

    字符串工具类加入补零的方法

---
+++ src/main/test/UtilTest.java
@@ -0,0 +1,16 @@
+import com.mbp.utils.StringExamUtil;
+
+/**
+ * FileName:UtilTest
+ * Author:  ghw06
+ * Date:    2018/4/3 13:58
+ * Description: 工具类的测试
+ */
+public class UtilTest {
+
+
+	public static void main(String[] args) {
+		String str = StringExamUtil.polishByZero(10, "456");
+		System.out.println(str);
+	}
+}
